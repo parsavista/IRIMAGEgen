77c41b986420b7710aee3d812e7d3f83ad95952a
parsavista <112246623+parsavista@users.noreply.github.com>
2025-06-09 00:42:45 +0330

    توضیح تغییرات

--- remove_webhook.py
+++ remove_webhook.py
@@ -1,9 +1,7 @@
 import requests
 
-bot_token = '7637390285:AAHv_nWEZcvbZ9yTnQCklcpkTn8X-dFiN5o'  # توکن بات شما
-
-url = f"https://api.telegram.org/bot{bot_token}/deleteWebhook"
-
+token = "7637390285:AAHv_nWEZcvbZ9yTnQCklcpkTn8X-dFiN5o"
+url = f"https://api.telegram.org/bot{token}/deleteWebhook"
 response = requests.post(url)
 print(response.json())
 
--- test_bot.py
+++ test_bot.py
@@ -1,10 +1,13 @@
 import telebot
 
-TOKEN = "7637390285:AAHv_nWEZcvbZ9yTnQCklcpkTn8X-dFiN5o"
+# توکن ربات رو اینجا با کوتیشن قرار بده
+TOKEN = '7637390285:AAHv_nWEZcvbZ9yTnQCklcpkTn8X-dFiN5o'
+
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "سلام! بات من آماده است.")
+    bot.reply_to(message, "سلام! ربات فعال شد.")
 
+# شروع polling برای دریافت پیام‌ها
 bot.polling()
